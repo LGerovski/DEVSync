f1e2e01083890f4fa67d472742db1dfee6db9843
LGerovski <lachezar_gerovski@abv.bg>
2024-08-13 00:14:06 +0300

    added ITesseract example. Included net.sourceforge.tess4j dependency in pom

--- myDev/src/main/java/syncCompany/App.java
+++ myDev/src/main/java/syncCompany/App.java
@@ -1,13 +1,27 @@
 package syncCompany;
 
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.io.File;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        ITesseract instance = new Tesseract();
+        instance.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata");
+        instance.setLanguage("eng");
+
+        try {
+            File imageFile = new File("C:\\Users\\LGerovski\\Pictures\\testTesseract.JPG");
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
     }
 }
