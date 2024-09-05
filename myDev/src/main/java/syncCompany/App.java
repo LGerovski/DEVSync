f912ee789b29083307c47c635b921479ff814166
gel1sf3 <lachezar.gerovski@bosch.com>
2024-09-05 17:53:55 +0300

    update

--- myDev/src/main/java/syncCompany/App.java
+++ myDev/src/main/java/syncCompany/App.java
@@ -13,11 +13,12 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         ITesseract instance = new Tesseract();
-        instance.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata");
+        //instance.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata");
+        instance.setDatapath("C:\\Users\\GEL1SF3\\Documents\\Pluralsight\\tesseract\\my-test\\src\\main\\settings\\tessdata");
         instance.setLanguage("eng");
 
         try {
-            File imageFile = new File("C:\\Users\\LGerovski\\Pictures\\testTesseract.JPG");
+            File imageFile = new File("C:\\Users\\GEL1SF3\\Pictures\\testTesseract.JPG");
             String result = instance.doOCR(imageFile);
             System.out.println(result);
         } catch (TesseractException e) {
