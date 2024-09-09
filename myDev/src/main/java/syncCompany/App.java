a9c9f5913c2ba0096acfc3cf190b1ab112aca897
LGerovski <lachezar_gerovski@abv.bg>
2024-09-10 00:46:10 +0300

    check and set path depends on workplace or personal computer, set custom DPI

--- myDev/src/main/java/syncCompany/App.java
+++ myDev/src/main/java/syncCompany/App.java
@@ -12,13 +12,18 @@ import java.io.File;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        File workPlace = new File("C:\\Users\\GEL1SF3\\Documents\\Pluralsight\\tesseract\\my-test\\src\\main\\settings\\tessdata");
+        String filepath = workPlace.exists() ? ("C:\\Users\\GEL1SF3\\Documents\\Pluralsight\\tesseract\\my-test\\src\\main\\settings\\tessdata") : ("C:\\Program Files\\Tesseract-OCR\\tessdata");
         ITesseract instance = new Tesseract();
         //instance.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata");
-        instance.setDatapath("C:\\Users\\GEL1SF3\\Documents\\Pluralsight\\tesseract\\my-test\\src\\main\\settings\\tessdata");
+        //instance.setDatapath("C:\\Users\\GEL1SF3\\Documents\\Pluralsight\\tesseract\\my-test\\src\\main\\settings\\tessdata");
+        instance.setDatapath(filepath);
         instance.setLanguage("eng");
+        instance.setTessVariable("user_defined_dpi", "96");
 
         try {
-            File imageFile = new File("C:\\Users\\GEL1SF3\\Pictures\\testTesseract.JPG");
+            //File imageFile = new File("C:\\Users\\GEL1SF3\\Pictures\\testTesseract.JPG");
+            File imageFile = new File("C:\\Users\\lgerovski\\Pictures\\testTesseract.JPG");
             String result = instance.doOCR(imageFile);
             System.out.println(result);
         } catch (TesseractException e) {
