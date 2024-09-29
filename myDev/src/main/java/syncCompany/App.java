ad9d798d6c37d3818971d1c38fbc307aee7d22a5
gel1sf3 <lachezar.gerovski@bosch.com>
2024-09-29 22:55:48 +0300

    Rework the pointing of the path to tessdata - create a temp directory, copy to this temp directory the files from the JAR

--- myDev/src/main/java/syncCompany/App.java
+++ myDev/src/main/java/syncCompany/App.java
@@ -1,44 +1,61 @@
 package syncCompany;
 
-import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
+
+
         //File workPlace = new File("C:\\Users\\GEL1SF3\\Documents\\Pluralsight\\tesseract\\my-test\\src\\main\\settings\\tessdata");
         //String filepath = workPlace.exists() ? ("C:\\Users\\GEL1SF3\\Documents\\Pluralsight\\tesseract\\my-test\\src\\main\\settings\\tessdata") : ("C:\\Program Files\\Tesseract-OCR\\tessdata");
         //String imagePath = workPlace.exists() ? ("C:\\Users\\GEL1SF3\\Pictures\\testTesseract.JPG"):("C:\\Users\\lgerovski\\Pictures\\testTesseract.JPG");
-        String filepath = null;
-        String imagePath = null;
-        try {
-            URL tessdataUrl = App.class.getClassLoader().getResource("tessdata");
-            URL picturesUrl = App.class.getClassLoader().getResource("testTesseract.JPG");
-            filepath = Paths.get(tessdataUrl.toURI()).toString();
-            imagePath = Paths.get(picturesUrl.toURI()).toString();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+        Path tempDir = Files.createTempDirectory("tessResources-temp");
+
+        InputStream tessdataStream = App.class.getClassLoader().getResourceAsStream("tessdata/eng.traineddata");
+        if (tessdataStream == null){
+            throw new RuntimeException("File not found in resources");
         }
-        ITesseract instance = new Tesseract();
-        instance.setDatapath(filepath);
+
+        Path tessdataDir = tempDir.resolve("tessdata");
+        Files.createDirectories(tessdataDir);
+
+        Path tessdataFile = tessdataDir.resolve("eng.traineddata");
+        Files.copy(tessdataStream, tessdataFile, StandardCopyOption.REPLACE_EXISTING);
+
+        InputStream testIMGStream = App.class.getClassLoader().getResourceAsStream("testTesseract.JPG");
+        if (testIMGStream == null){
+            throw new RuntimeException("File not found in resources");
+        }
+
+        Path testIMGFile = tempDir.resolve("testTesseract.JPG");
+        Files.copy(testIMGStream, testIMGFile, StandardCopyOption.REPLACE_EXISTING);
+
+        Tesseract instance = new Tesseract();
+        instance.setDatapath(tempDir.resolve("tessdata").toString());
         instance.setLanguage("eng");
+
         instance.setTessVariable("user_defined_dpi", "96");
 
         try {
-            File imageFile = new File(imagePath);
-            String result = instance.doOCR(imageFile);
+            String result = instance.doOCR(testIMGFile.toFile());
             System.out.println(result);
-        } catch (TesseractException e) {
+        }catch (TesseractException e){
             e.printStackTrace();
         }
+
+        tessdataStream.close();;
+        testIMGStream.close();
+        System.out.println("pass8");
     }
 }
